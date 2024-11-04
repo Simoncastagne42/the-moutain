04f330b3791dafdb160aa261daa3c61d760e04d4
Simoncastagne42 <simoncastagne42@gmail.com>
2024-11-04 17:00:17 +0100

    fin section 3

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    colors: {
+      'green': '#40C6C5',
+      'white': '#FEFFFF',
+      'gris': '#E0EDF6',
+      'rouge': '#F52257',
+      'gris-bis': '#5A546B',
+      'bleu': '#8CCFEA',
+    },
+
+    
+    extend: {},
+  },
+  plugins: [],
+}
+
